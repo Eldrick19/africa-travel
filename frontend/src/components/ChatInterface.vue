9808cc021845f572bd558d2489eea6350e74b291
Eldrick19 <eldrick19@github.com>
2023-10-27 02:42:42 +0000

    Some changes (add VueJS)

---
+++ frontend/src/components/ChatInterface.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <div v-for="(message, index) in messages" :key="index">
+        {{ message }}
+      </div>
+      <input v-model="inputMessage" @keyup.enter="sendMessage" placeholder="Enter a message">
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        inputMessage: '',
+        messages: []
+      }
+    },
+    methods: {
+      sendMessage() {
+        this.messages.push(this.inputMessage)
+        this.inputMessage = ''
+        this.messages.push('Response')
+      }
+    }
+  }
+  </script>
